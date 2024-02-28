f889317ef313c58cb1089a2fd12d4f6366640dfe
IlhamJepri <ilhamjepri16@gmail.com>
2024-02-28 10:35:55 +0700

    first commit

---
+++ array._asosiatif.php
@@ -0,0 +1,10 @@
+<?php
+$mhs1 = [
+    'nama' => 'Ilham Jepri', 
+    'nim' => '0110
+    ', 
+    'jurusan' => 'Sistem Informasi',
+];
+
+// memanggil array asosiatif
+echo 'nama: ' . $mhs1['nama'];
